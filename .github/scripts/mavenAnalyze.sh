d70c2b46e3adc35ba6f44b79233e330e8eed1239
Sebastian Piazza <sebastian@adermarkarian23gmail.onmicrosoft.com>
2023-07-06 22:29:58 -0300

    update script

--- .github/scripts/mavenAnalyze.sh
+++ .github/scripts/mavenAnalyze.sh
@@ -1,1 +1,1 @@
-mvnw -B verify sonar:sonar -Dsonar.projectKey=$PROJECT_KEY -Dsonar.organization=$ORG_NAME -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN
+./mvnw -B verify sonar:sonar -Dsonar.projectKey=$PROJECT_KEY -Dsonar.organization=$ORG_NAME -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN
