a321547558ae7439655a7b597ef4e31fff8c225d
Sebastian Piazza <89656196+SebaORT@users.noreply.github.com>
2023-07-18 21:01:53 -0300

    added quality gate check (#28)
    Co-authored-by: Sebastian Piazza <sebastian@adermarkarian23gmail.onmicrosoft.com>

--- .github/scripts/mavenAnalyze.sh
+++ .github/scripts/mavenAnalyze.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./mvnw -B verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=$PROJECT_KEY -Dsonar.organization=$ORG_NAME -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN
+./mvnw -B verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.qualitygate.wait=true -Dsonar.projectKey=$PROJECT_KEY -Dsonar.organization=$ORG_NAME -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN
 
 # mvn -Dsonar.coverage.jacoco.xmlReportPaths=
 #       $JACOCO_XML
